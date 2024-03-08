86b8c0a67af3dfccb1c8a3855bbda31da21577cc
Jordan Walters <128825567+Jordan-Walters-23@users.noreply.github.com>
2024-03-08 17:50:30 +0000

    Responsive Design

--- src/components/Page1Nav.tsx
+++ src/components/Page1Nav.tsx
@@ -5,12 +5,12 @@ import Link from 'next/link'
 function Page1Nav() {
   return (
     <>
-      <button className='m-2 h-10 w-4/6 rounded-full bg-teal-900 px-4 py-2 font-bold text-teal-50 hover:bg-teal-950'>
+      <button className='h-10 w-4/6 sm:w-4/6 md:w-4/6 lg:w-500 2lx:w-500 m-1 rounded-full bg-teal-900 px-4 py-2 font-bold text-teal-50 hover:bg-teal-950'>
         +
       </button>
       <Link
         href='/guess'
-        className='h-10 w-1/3 rounded-full bg-teal-100 px-4 py-2 text-black hover:bg-emerald-100'
+        className='h-10 w-40 sm:w-40 md:w-40 lg:w-96 m-1 rounded-full text-center bg-teal-100 px-4 py-2 text-black hover:bg-emerald-100'
       >
         Play
       </Link>
--- src/components/Page2Nav.tsx
+++ src/components/Page2Nav.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 function Page2Nav() {
   return (
     <>
-      <button type="reset" className='h-10 w-1/3 rounded-full bg-teal-100 px-4 py-2 text-black hover:bg-emerald-100'>Guess again</button>
-      <Link href="/" className='h-10 w-1/3 rounded-full text-center bg-teal-100 px-4 py-2 text-black hover:bg-emerald-100'>New songs</Link>
+      <button type="reset" className='h-10 w-40 sm:w-40 md:w-40 lg:w-96 lx:w-1/3 2lx:w-1/3 m-1 rounded-full bg-teal-100 text-black hover:bg-emerald-100'>Guess again</button>
+      <Link href="/" className='h-10 w-40 sm:w-40 md:w-40 lg:w-96 lx:w-1/3 2lx:w-1/3 m-1 rounded-full text-center bg-teal-100 text-black hover:bg-emerald-100'>New songs</Link>
     </>
   );
 }
