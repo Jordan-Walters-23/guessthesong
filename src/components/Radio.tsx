57fab3a7aa9dcc6f83d33fb8ae6849c0c86fb9cd
Jordan Walters <128825567+Jordan-Walters-23@users.noreply.github.com>
2024-03-12 12:19:20 +0000

    Assign type to props

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -1,23 +1,35 @@
 'use client'
-import { createContext } from "react"
-import { useContext } from "react"
+import { createContext } from 'react'
+import { useContext } from 'react'
 import React from 'react'
 
 const RadioContext = createContext(null)
 
-export default function Radio({children, ...props }) {
-    const {value, onChange} = useContext(RadioContext)
+interface Props {
+  children?: ReactNode
+  // type for any props that come into the component
+}
+
+export default function Radio({ children, ...props }) {
+  const { value, onChange } = useContext(RadioContext)
   return (
-    <label><input type="radio" className="hidden" checked={value === props.value} onChange={onChange} {...props}/>
-        {children}
+    <label>
+      <input
+        type='radio'
+        className='hidden'
+        checked={value === props.value}
+        onChange={onChange}
+        {...props}
+      />
+      {children}
     </label>
   )
 }
 
-export function RadioGroup({value, onChange, children}) {
-    return (
-        <RadioContext.Provider value={{ value, onChange }}>
-            {children}
-        </RadioContext.Provider>
-    )
+export function RadioGroup({ value, onChange, children }) {
+  return (
+    <RadioContext.Provider value={{ value, onChange }}>
+      {children}
+    </RadioContext.Provider>
+  )
 }
