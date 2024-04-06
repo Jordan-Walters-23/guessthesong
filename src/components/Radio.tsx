4081d087b3a325b8f50683077fd153bac5e4adb3
Jordan Walters <128825567+Jordan-Walters-23@users.noreply.github.com>
2024-04-06 13:04:08 +0100

    Stlying song Selector Feature Two custom types (interfaces) were used. Selector button highlight feature and "Selected" text display

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -1,24 +1,31 @@
 'use client'
-import React, { createContext, useContext, ReactNode, ChangeEvent } from 'react';
+import React, { createContext, useContext, ReactNode, ChangeEvent } from 'react'
 
 interface RadioProps {
-  value: string;
-  children: ReactNode;
+  value: string
+  children: ReactNode
 }
 
 interface RadioGroupProps {
-  value: string;
-  onChange: (value: string) => void;
-  children: ReactNode;
+  value: string
+  onChange: (value: string) => void
+  children: ReactNode
 }
 
-const RadioContext = createContext<{ value: string; onChange: (value: string) => void } | null>(null);
+const RadioContext = createContext<{
+  value: string
+  onChange: (value: string) => void
+} | null>(null)
 
-function Radio({ value, children }: RadioProps) {
-  const { value: contextValue, onChange: contextOnChange } = useContext(RadioContext) || { value: '', onChange: () => {} };
+function Radio({ value, children }: RadioPropsRadioGroupProps) {
+  const { value: contextValue, onChange: contextOnChange } = useContext(
+    RadioContext
+  ) || { value: '', onChange: () => {} }
 
   return (
-    <label>
+    <label
+  className={`cursor-pointer rounded-lg px-6 py-4 shadow transition-all ${value === contextValue ? 'bg-gradient-to-t from-blue-200 to-blue-100 text-blue-800 shadow-blue-500' : 'shadow-gray-300 hover:shadow-md'}`}
+    >
       <input
         type='radio'
         className='hidden'
@@ -27,15 +34,19 @@ function Radio({ value, children }: RadioProps) {
       />
       {children}
     </label>
-  );
+  )
 }
 
-Radio.Group = function RadioGroup({ value, onChange, children }: RadioGroupProps) {
+Radio.Group = function RadioGroup({
+  value,
+  onChange,
+  children
+}: RadioGroupProps) {
   return (
     <RadioContext.Provider value={{ value, onChange }}>
       {children}
     </RadioContext.Provider>
-  );
-};
+  )
+}
 
 export default Radio
