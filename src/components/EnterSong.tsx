8aab4fe8ee17e91e4ca443a596be9e54f4265cb8
Jordan Walters <128825567+Jordan-Walters-23@users.noreply.github.com>
2024-06-26 21:22:54 +0100

    Started to build embeded song retrival

--- src/components/EnterSong.tsx
+++ src/components/EnterSong.tsx
@@ -5,9 +5,9 @@ function EnterSong() {
     <>
       <h2><b>Add your songs...</b></h2>
       <div className='grid place-item-center w-5/6 p-5 rounded-3xl bg-teal-600'>
-        <input className='bg-teal-600' name="myInput" defaultValue="Artist..." />
-        <input className='bg-teal-600' name="myInput" defaultValue="Song..." />
-        <input className='bg-teal-600' name="myInput" defaultValue="Lyrics.." />
+        <input className='bg-teal-600' name="myInput" placeholder="Song link..." />
+        {/* <input className='bg-teal-600' name="myInput" placeholder="Song..." /> */}
+        {/* <input className='bg-teal-600' name="myInput" placeholder="Lyrics.." /> */}
       </div>
     </>
   )
